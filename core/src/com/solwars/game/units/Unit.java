a6aaa1936c5994edac68fba83052ff6c67e86c7c
Unknown <daniel.fowke@adacollege.org.uk>
2017-11-01 10:54:48 +0000

    Quick change forgot to commit

--- core/src/com/solwars/game/units/Unit.java
+++ core/src/com/solwars/game/units/Unit.java
@@ -7,23 +7,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * Created by Student on 18/10/2017.
  */
 public class Unit{
-    SteeringAgentRedundant steeringAgent;
-
-
-    public void setSteeringAgent(SteeringAgentRedundant steeringAgent) {
-        this.steeringAgent = steeringAgent;
-    }
-    public Unit(){
-        //steeringAgent.setMaxAngularSpeed(10f);
-        //steeringAgent.setMaxLinearSpeed(20f);
-    }
-    public void draw(SpriteBatch batch, Sprite sprite){
-        sprite.setX(steeringAgent.position.x);
-        sprite.setY(steeringAgent.position.y);
-        sprite.setRotation(steeringAgent.vectorToAngle(steeringAgent.getLinearVelocity()));
-        sprite.draw(batch);
-
-    }
 
 
 
