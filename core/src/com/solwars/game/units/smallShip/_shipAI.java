9a021356d62c62c492670c87eb277a4a8a54f4e5
Unknown <daniel.fowke@adacollege.org.uk>
2017-11-06 14:21:51 +0000

    Read last commit

--- core/src/com/solwars/game/units/smallShip/_shipAI.java
+++ core/src/com/solwars/game/units/smallShip/_shipAI.java
@@ -1,21 +1,8 @@
-package com.solwars.game.ai;
-
-import com.badlogic.gdx.math.Vector2;
-import com.solwars.game.units.Unit;
+package com.solwars.game.units.smallShip;
 
 /**
  * Created by Student on 03/11/2017.
  */
-public class shipAI implements _AI {
+public class _shipAI {
 
-    Unit owner;
-
-    public shipAI(Unit owner){
-        this.owner = owner;
-    }
-
-    @Override
-    public void setTarget(Vector2 Target) {
-
-    }
 }
